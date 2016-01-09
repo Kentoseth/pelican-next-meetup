58e898695174050fc410396b6dc15f985e855a1c
Arnis Jaundžeikars <sangsatori@radiantmoons.me>
2016-01-09 17:39:26 +0200

    Write initial version of the plugin

---
+++ next_meetup/__init__.py
@@ -0,0 +1,12 @@
+"""Pelican plugin to get info on your next meetup."""
+from .next_meetup import register
+
+__title__ = 'pelican_next_meetup'
+__version__ = '0.1.0'
+__author__ = 'Arnis Jaundžeikars'
+__credits__ = ['Arnis Jaundžeikars']
+__maintainer__ = 'Arnis Jaundžeikars'
+__email__ = '<sangsatori@radiantmoons.me>'
+__status__ = "Alpha"
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016'
