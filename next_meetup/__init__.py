b523406f58541ffa92228ae6dfe00b867f29a081
Arnis Jaundžeikars <sangsatori@radiantmoons.me>
2016-01-09 18:10:00 +0200

    Remove needless metadata

--- next_meetup/__init__.py
+++ next_meetup/__init__.py
@@ -1,12 +1,2 @@
 """Pelican plugin to get info on your next meetup."""
 from .next_meetup import register
-
-__title__ = 'pelican-next-meetup'
-__version__ = '0.1.0'
-__author__ = 'Arnis Jaundžeikars'
-__credits__ = ['Arnis Jaundžeikars']
-__maintainer__ = 'Arnis Jaundžeikars'
-__email__ = '<sangsatori@radiantmoons.me>'
-__status__ = "Alpha"
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2016'
