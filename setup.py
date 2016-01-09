f3a37ed8b693c8a4efe2d866c16ccc50a2cce277
Arnis Jaundžeikars <sangsatori@radiantmoons.me>
2016-01-09 18:23:53 +0200

    Add a missing comma

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
 
-        'Topic :: Internet :: WWW/HTTP'
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     keywords='pelican plugins meetup',
